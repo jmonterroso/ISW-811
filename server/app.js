aff26aaf38250604180c0d264b8af1f6eb4b7065
Jayson Monterroso <1420938+jmonterroso@users.noreply.github.com>
2021-09-23 08:15:50 -0600

    adding morgan to project

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 const dotEnv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const logger = require("morgan");
 const app = express();
+
 // esta linea ayuda a leer la configuracion que tenemos en el archivo .env
 dotEnv.config();
 
-// se define el puerto que va a escuchar basado en el archivo de configuración
+// se define el puerto que va a escuchar basado en el archivo de configuración .env
 const port = process.env.PORT || 3000;
 
 // usamos el middleware cors para aceptar llamadas cors en nuestro servidor
 app.use(cors());
+app.use(logger("dev"));
 
 // iniciamos nuestro servidor
 app.listen(port, () => {
